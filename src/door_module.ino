3ec4602a2dab8cc608d4fd266b406f19e59652ea
Rudgas <birk.urmersbach@gmx.de>
2022-11-15 17:14:12 +0100

    hw7 adjustments, accelstepper prep

--- src/door_module.ino
+++ src/door_module.ino
@@ -47,18 +47,14 @@ D19 - SCL
 
 *///----------------------------------------------------------------------------
 
-#include <Arduino.h>
 #include <Wire.h>
-#include <TMC2130Stepper.h>
-#include <Adafruit_DotStar.h>
-
-const uint8_t has_lock = 0;
+#include <TMCStepper.h>
 
 //Stepper 1
-const uint8_t S1_dir	=	0;
-const uint8_t S1_step	=	4; //port PA08
-const uint8_t S1_EN		=	2;
-const uint8_t S1_CS		=	3;
+const uint8_t S1_dir	=	3;
+const uint8_t S1_step	=	22; //port PA08
+const uint8_t S1_EN		=	23;
+const uint8_t S1_CS		=	2;
 const uint8_t S1_microsteps = 32;
 
 volatile uint8_t S1_busy = 0; //Is stepper moving
@@ -68,46 +64,44 @@ int16_t steps_last_move;      //holds current position in steps when move is int
 
 //Stepper 2
 const uint8_t S2_dir	=	10;
-const uint8_t S2_step	=	11; //port PA16
-const uint8_t S2_EN		=	12;
-const uint8_t S2_CS		=	1;
+const uint8_t S2_step	=	8; //port PA16
+const uint8_t S2_EN		=	9;
+const uint8_t S2_CS		=	7;
 const uint8_t S2_microsteps = 32;
 
 uint8_t S2_busy = 0;      //Is stepper moving
-uint8_t door_locked = 1;  //is door locked
+
 
 //IR Sensor Pins
-const uint8_t IR_top = A2;
-const uint8_t IR_upper = 9;
-const uint8_t IR_middle = A4;
-const uint8_t IR_lower = A5;
-const uint8_t IR_bottom = A3;
+const uint8_t IR1_tx1 = 1;
+const uint8_t IR1_tx2 = 0;
+const uint8_t IR1_rx1 = 21;
+const uint8_t IR1_rx2 = 24;
+const uint8_t IR1_top = 25;
+const uint8_t IR1_bottom = 4;
 
-const uint8_t IR_barrier_rx = A1;
-const uint8_t IR_barrier_tx = A0;
+const uint8_t IR2_tx1 = 6;
+const uint8_t IR2_tx2 = 5;
+const uint8_t IR2_rx1 = 20;
+const uint8_t IR2_rx2 = 15;
+const uint8_t IR2_top = 16;
+const uint8_t IR2_bottom = 17;
 
-const uint8_t IR_all[7] = {IR_top, IR_upper, IR_middle, IR_lower, IR_bottom, IR_barrier_rx, IR_barrier_tx}; //1=blocked
-volatile uint8_t IR_state[7] = {0,0,0,0,0,0,0};
+//const uint8_t IR_all[7] = {IR_top, IR_upper, IR_middle, IR_lower, IR_bottom, IR_barrier_rx, IR_barrier_tx}; //1=blocked
+//volatile uint8_t IR_state[7] = {0,0,0,0,0,0,0};
 
 //to improve function readability
 const uint8_t top = 0;    //IR LEDs
-const uint8_t upper = 1;
-const uint8_t middle = 2;
-const uint8_t lower = 3;
-const uint8_t bottom = 4;
-const uint8_t rx = 5;
-const uint8_t tx = 6;
+const uint8_t bottom = 1;
+const uint8_t rx = 2;
+const uint8_t tx = 3;
 
 const uint8_t up = 0;   //door direction
 const uint8_t down = 1;
 
-const uint8_t close = 1; //lock direction
-const uint8_t open = 0;
-const uint16_t lock_move_steps = 192;
-
 //LEDs
-const uint8_t LED1 = 7;
-const uint8_t LED2 = 5;
+const uint8_t LED1 = 14;
+const uint8_t LED2 = 26;
 
 uint8_t closedoor = 1;
 
@@ -118,37 +112,76 @@ volatile uint16_t Q_movesimple[4]; //queue a movesimple command to release I2C b
 volatile uint16_t Q_vibrate[4];
 volatile uint8_t newcommand = 0;   //flag for abandoning current movement
 
-Adafruit_DotStar strip(1, 41, 40, DOTSTAR_BRG); //create dotstar object
+TMC5160Stepper driver1(S1_CS, 0.075f, 11, 12, 13); //(CS, Rsens, MOSI, MISO, SCK)
+TMC5160Stepper driver2(S2_CS, 0.075f, 11, 12, 13);
 
-TMC2130Stepper driver1 = TMC2130Stepper(S1_EN, S1_dir, S1_step, S1_CS);
-TMC2130Stepper driver2 = TMC2130Stepper(S2_EN, S2_dir, S2_step, S2_CS);
 
+/*
+AccelStepper Xaxis(1, 3, 6); // pin 3 = step, pin 6 = direction
+AccelStepper Yaxis(1, 4, 7); // pin 4 = step, pin 7 = direction
+ Xaxis.setMaxSpeed(400);
+  Yaxis.setMaxSpeed(400);
+ Xaxis.setSpeed(45);
+  Yaxis.setSpeed(25);
+ Xaxis.runSpeed();
+   Yaxis.runSpeed();
+
+
+
+mystepper.moveTo(targetPosition);
+Move the motor to a new absolute position. This returns immediately. Actual movement is caused by the run() function.
+
+mystepper.move(distance);
+Move the motor (either positive or negative) relative to its current position. This returns immediately. Actual movement is caused by the run() function.
+
+mystepper.currentPosition();
+Read the motor's current absolution position.
+
+mystepper.distanceToGo();
+Read the distance the motor is from its destination position. This can be used to check if the motor has reached its final position.
+
+mystepper.run();
+Update the motor. This must be called repetitively to make the motor move.
+
+mystepper.runToPosition();
+Update the motor, and wait for it to reach its destination. This function does not return until the motor is stopped, so it is only useful if no other motors are moving.
+
+Speed Based Control
+mystepper.setSpeed(stepsPerSecond);
+Set the speed, in steps per second. This function returns immediately. Actual motion is caused by called runSpeed().
+
+mystepper.runSpeed();
+Update the motor. This must be called repetitively to make the motor move.
+
+
+
+
+*/
 //##############################################################################
 //#####   S E T U P   ##########################################################
 //##############################################################################
 void setup(){
-  //Set up RGB LED on board, and turn it off
-  strip.begin(); //Initialize pins for output
-  strip.show();  //Turn dotstar LED off
-  //while(!Serial);
+  //while(!Serial); //wait for serial connection
   
   //Setup IR Sensors (high when interrrupted)
-  pinMode(IR_top,INPUT);
-  pinMode(IR_upper,INPUT);
-  pinMode(IR_middle,INPUT);
-  pinMode(IR_lower,INPUT);
-  pinMode(IR_bottom,INPUT);
+  pinMode(IR1_top,INPUT);
+  pinMode(IR1_bottom,INPUT);
+  pinMode(IR1_tx1,INPUT);
+  pinMode(IR1_tx2,INPUT);
+  pinMode(IR1_rx1,INPUT);
+  pinMode(IR1_rx2,INPUT);
   
-  pinMode(IR_barrier_rx,INPUT);
-  pinMode(IR_barrier_tx,INPUT);
-  
+  pinMode(IR2_top,INPUT);
+  pinMode(IR2_bottom,INPUT);
+  pinMode(IR2_tx1,INPUT);
+  pinMode(IR2_tx2,INPUT);
+  pinMode(IR2_rx1,INPUT);
+  pinMode(IR2_rx2,INPUT);
+
   // IR debug
   // while(1)
   // {
   //   Serial.print(digitalRead(IR_top));
-  //   Serial.print(digitalRead(IR_upper));
-  //   Serial.print(digitalRead(IR_middle));
-  //   Serial.print(digitalRead(IR_lower));
   //   Serial.print(digitalRead(IR_bottom));
   //   Serial.print("-");
   //   Serial.print(analogRead(A1));
@@ -168,32 +201,30 @@ void setup(){
   
   //Setup stepper drivers
   driver1.begin(); //initialize pins and registries
-	driver1.rms_current(100,0.11,0.4); //set driving current RMS (current RMS, Rsens, hold multiplier) (300max for NEMA 8)
-  driver1.stealthChop(1); //enable stealthchopping for quiet runnning
+	driver1.rms_current(100,0.4); //set driving current RMS (current RMS, hold multiplier) (300max for NEMA 8)
+  driver1.en_pwm_mode(true);  //enable stealthchopping for quiet runnning
 	driver1.microsteps(S1_microsteps); //default 256 for quiet running
-  driver1.interpolate(1);
-  driver1.double_edge_step(1);
+  driver1.pwm_autoscale(true);     // Needed for stealthChop
+  driver1.dedge(true);  //step on both edges of the signal
 
   driver2.begin(); //initialize pins and registries
-	driver2.rms_current(25,0.11,0.2); //set driving current RMS (current RMS, Rsens, hold multiplier) (300max for NEMA 8)
-  driver2.stealthChop(1); //enable stealthchopping for quiet runnning
-	driver2.microsteps(S2_microsteps); //default 256 for quiet running
-  driver2.interpolate(1);
-  driver2.double_edge_step(1); //step on rising and falling edge
+	driver2.rms_current(100,0.4); //set driving current RMS (current RMS, hold multiplier) (300max for NEMA 8)
+  driver2.en_pwm_mode(true);  //enable stealthchopping for quiet runnning
+	driver2.microsteps(S1_microsteps); //default 256 for quiet running
+  driver2.pwm_autoscale(true);     // Needed for stealthChop
+  driver2.dedge(true);  //step on both edges of the signal
   
-  pinMode(S1_step,OUTPUT);
-  pinMode(S2_step,OUTPUT);
+  //pinMode(S1_step,OUTPUT);
+  //pinMode(S2_step,OUTPUT);
   
-	digitalWrite(S1_EN,LOW); //Enable output on drivers
-  digitalWrite(S2_EN,LOW); //Enable output on drivers
+	//digitalWrite(S1_EN,LOW); //Enable output on drivers
+  //digitalWrite(S2_EN,LOW); //Enable output on drivers
   
   //Stepper driver debug
   // Serial.println(driver1.GCONF(), BIN);
   // Serial.println(driver2.GCONF(), BIN);
 
   //----- calibrate movements --------------------------------------------------
-  //move lock
-  movelock(open,250,lock_move_steps);   //move the open distance once to make sure it is open before moving the door
 
   //repeat closing calibration until two consecutive moves report roughly the same number of steps. perfect conditions vary by ~4 steps
   Serial.println("calibrate");
@@ -204,9 +235,6 @@ void setup(){
     steps_to_close = movesimple(down,bottom,S1_pulsetime,1); //and back down so it starts in closed configuration
     //Serial.println(steps_to_close);
   }
-  
-  movelock(close,1000,2112);            //move much more than close distance to make sure we are correctly positioned
-  movelock(open,1000,32);               //open a bit to prevent coil whine from stepper being under tension from pressing against mount
   
   //----- start I2C on address 0x11 --------------------------------------------
   Wire.begin(0x11);             //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -231,7 +259,8 @@ void loop(){
     //down-movement will always be step-based, up-movement, always feedback based
     steps_last_move = movesimple(Ql_movesimple[1],Ql_movesimple[2],Ql_movesimple[3],!Ql_movesimple[1]); //dir,target,pulsetime,IR based(1) or step(0)
     
-    if(!Q_movesimple[0]) S1_busy = 0;}
+    if(!Q_movesimple[0]) S1_busy = 0;
+  }
     
   //------ perform queued vibration of door ------------------------------------
   if(Q_vibrate[0]){
@@ -246,34 +275,30 @@ void loop(){
     for(uint8_t h = 0; h <= (Q_vibrate[3]*2); h++){
       digitalWrite(S1_dir, direction);
       for(uint8_t i = 0; i <= Q_vibrate[1]; i++){
-        move(Q_vibrate[2]);}
-      direction = !direction;}
+        move(Q_vibrate[2]);
+      }
+      direction = !direction;
+    }
 
     movesimple(down,bottom,S1_pulsetime,1);
     
-    S1_busy = 0;}
+    S1_busy = 0;
+  }
 } //end of loop
 
 //##############################################################################
 //#####   F U N C T I O N S   ##################################################
 //##############################################################################
 
-void movelock(uint8_t direction, uint16_t pulsetime, uint16_t steps){
-  if(has_lock){
-    digitalWrite(S2_dir, direction); //0 open, 1 close
-    
-    for(uint16_t i=0; i<steps; i++){
-      REG_PORT_OUTSET0 = PORT_PA16; // ~0.4us stepper 1
-      delayMicroseconds(pulsetime);
-      REG_PORT_OUTCLR0 = PORT_PA16; // ~0.4us
-      delayMicroseconds(pulsetime);}}}
-
 //move two microsteps
 void move(uint16_t pulsetime){
-  REG_PORT_OUTSET0 = PORT_PA08; // ~0.4us stepper 1
+  //REG_PORT_OUTSET0 = ; // ~0.4us stepper 1
+  digitalWriteFast(S1_step,HIGH);
   delayMicroseconds(pulsetime);
-  REG_PORT_OUTCLR0 = PORT_PA08; // ~0.4us
-  delayMicroseconds(pulsetime);}
+  //REG_PORT_OUTCLR0 = ; // ~0.4us
+  digitalWriteFast(S1_step,LOW);
+  delayMicroseconds(pulsetime);
+}
 
 //------------------------------------------------------------------------------
 //move door to target position, if door is blocked will retry movement
@@ -298,22 +323,27 @@ uint16_t movesimple(uint8_t direction, uint8_t target, uint16_t pulsetime, uint8
       if(direction && (IR_state[rx] || IR_state[tx])){
         direction = up;                  //change dir to up
         digitalWrite(S1_dir, direction); //0 up, 1 down
-        temp_target = top;}              //retry target is top <-- can be changed for different target e.g. only one step up, or half open
+        temp_target = top; //retry target is top <-- can be changed for different target e.g. only one step up, or half open
+      }
       
       //if we are at the retry target after a retry and not blocked, move back to original target
       if(!(IR_state[temp_target] ^ direction) && (target != temp_target) && !IR_state[rx] && !IR_state[tx]){
         direction = down;                  //change dir to down
         digitalWrite(S1_dir, direction);   //0 up, 1 down
-        temp_target = target;}
+        temp_target = target;
+      }
       
       //if not at defined target, move
       if(IR_state[temp_target] ^ direction){
         move(pulsetime);
         if(direction == up) steps_counted--;
-        else steps_counted++;}
+        else steps_counted++;
+      }
       
       //if we have reached our target we are done
-      if(!(IR_state[temp_target] ^ direction) && (target == temp_target)) done = 1;}}
+      if(!(IR_state[temp_target] ^ direction) && (target == temp_target)) done = 1;
+    }
+  }
   else{ //Step based movement
     while(!done){
       if(Q_movesimple[0]) done = 1; //stop current operation if new movement command
@@ -324,29 +354,33 @@ uint16_t movesimple(uint8_t direction, uint8_t target, uint16_t pulsetime, uint8
       //if barrier is blocked on moving down, change direction
       if(direction && (IR_state[rx] || IR_state[tx])){
         direction = up;                    //change dir to up
-        digitalWrite(S1_dir, direction);}  //0 up, 1 down
-      
+        digitalWrite(S1_dir, direction);  //0 up, 1 down
+      }
       //move all the way to the top if IR is blocked
       //if((steps_counted <= 0) && !IR_state[rx] && !IR_state[tx])
       //if we are moving up or we have reached the top, change direction to down but only if IR is not blocked
       if(((direction == up) || (steps_counted <= 0)) && (!IR_state[rx] && !IR_state[tx])){
         direction = down;                 //change dir to down
-        digitalWrite(S1_dir, direction);} //0 up, 1 down
-      
+        digitalWrite(S1_dir, direction); //0 up, 1 down
+      }
       //move up if blocked and down only if not blocked
       if((direction == down) || ((direction == up) && !(steps_counted <= 0))){
         if(direction == up){ //when moving up, move up slower
           steps_counted--;
-          move(5000);}       //very slow up movement
+          move(5000); //very slow up movement
+        }       
         else{                //down movement at normal speed
           steps_counted++;
-          move(pulsetime);}}
+          move(pulsetime);
+        }
+      }
       
       //if we made the number of calibrated steps for down movement, we are done
       if(steps_counted >= steps_to_close + 5) done = 1;}} //add a few more steps to counter drift
   
   //done with movement
-  return steps_counted;}
+  return steps_counted;
+}
 
 //------------------------------------------------------------------------------
 //close step by step with timing and feedback
@@ -366,22 +400,28 @@ void closefancy(uint8_t start, uint8_t stop, uint16_t pulsetime){
       move_time = millis();
       while((digitalRead(IR_all[i+1]) == 0) && lower_door){
         if((millis() - move_time) < (move_interval_times[i] * 1.2)){
-          move(pulsetime);}
-        else lower_door = 0;}}
+          move(pulsetime);
+        }
+        else lower_door = 0;
+      }
+    }
 
-      //move up to reset if door doesn't reach target within time
-      //Problem: If door is already above sensor, it will start moving down again, potentially
-      //unravelling the string
-      //solution 1: always move all the way up
-      //solution 2: use movement timing <- now
-      digitalWrite(S1_dir, up); //move up
-      move_time = millis();
-      //even if IR is already unblocked (mouse lifts door up) move back a bit to prevent string from uncurling
-      while(((digitalRead(IR_all[i]) == 1) || ((millis() - move_time) < (move_interval_times[i] * 1.1))) && !lower_door){
-        move(pulsetime);}
-      //if lower_door true, move finished, else door moved up, try again
-      if(lower_door) closing = 0;
-      lower_door = 1;}}
+    //move up to reset if door doesn't reach target within time
+    //Problem: If door is already above sensor, it will start moving down again, potentially
+    //unravelling the string
+    //solution 1: always move all the way up
+    //solution 2: use movement timing <- now
+    digitalWrite(S1_dir, up); //move up
+    move_time = millis();
+    //even if IR is already unblocked (mouse lifts door up) move back a bit to prevent string from uncurling
+    while(((digitalRead(IR_all[i]) == 1) || ((millis() - move_time) < (move_interval_times[i] * 1.1))) && !lower_door){
+      move(pulsetime);
+    }
+    //if lower_door true, move finished, else door moved up, try again
+    if(lower_door) closing = 0;
+    lower_door = 1;
+  }
+}
 
 //------------------------------------------------------------------------------
 //Do a calibration movement where we time how long it takes the door to reach each IR barrier point
@@ -394,7 +434,8 @@ void calibrate(uint16_t pulsetime){
     REG_PORT_OUTSET0 = PORT_PA08; // ~0.4us stepper 1
     delayMicroseconds(pulsetime);
     REG_PORT_OUTCLR0 = PORT_PA08; // ~0.4us
-    delayMicroseconds(pulsetime);}
+    delayMicroseconds(pulsetime);
+  }
   move_interval_times[0] = millis() - move_time;
   delay(100);
 
@@ -403,7 +444,8 @@ void calibrate(uint16_t pulsetime){
     REG_PORT_OUTSET0 = PORT_PA08; // ~0.4us stepper 1
     delayMicroseconds(pulsetime);
     REG_PORT_OUTCLR0 = PORT_PA08; // ~0.4us
-    delayMicroseconds(pulsetime);}
+    delayMicroseconds(pulsetime);
+  }
   move_interval_times[1] = millis() - move_time;
   delay(100);
   
@@ -412,7 +454,8 @@ void calibrate(uint16_t pulsetime){
     REG_PORT_OUTSET0 = PORT_PA08; // ~0.4us stepper 1
     delayMicroseconds(pulsetime);
     REG_PORT_OUTCLR0 = PORT_PA08; // ~0.4us
-    delayMicroseconds(pulsetime);}
+    delayMicroseconds(pulsetime);
+  }
   move_interval_times[2] = millis() - move_time;
   delay(100);
   
@@ -421,7 +464,8 @@ void calibrate(uint16_t pulsetime){
     REG_PORT_OUTSET0 = PORT_PA08; // ~0.4us stepper 1
     delayMicroseconds(pulsetime);
     REG_PORT_OUTCLR0 = PORT_PA08; // ~0.4us
-    delayMicroseconds(pulsetime);}
+    delayMicroseconds(pulsetime);
+  }
   move_interval_times[3] = millis() - move_time;
   delay(100);
   
@@ -445,7 +489,8 @@ void receiveEvent(int bytes_incoming){
   //collect bytes from I2C
   for(uint8_t i = 0; i < bytes_incoming; i++){
     uint8_t c = Wire.read();
-    inputbuffer[i] = c;}
+    inputbuffer[i] = c;
+  }
   
   //----- process received data -----
   option = inputbuffer[0];
@@ -464,7 +509,8 @@ void receiveEvent(int bytes_incoming){
     S1_pulsetime = S1_pulsetime | inputbuffer[2];
     S1_pulsetime = (S1_pulsetime << 8) | inputbuffer[1];
     
-    calibrate(S1_pulsetime);}
+    calibrate(S1_pulsetime);
+  }
   
   if(option == 2){ //move door with simple feedback
     Q_movesimple[1] = inputbuffer[1];                           //direction
@@ -474,19 +520,23 @@ void receiveEvent(int bytes_incoming){
     Q_movesimple[3] = (Q_movesimple[3] << 8) | inputbuffer[3];  //pulsetime
     
     Q_movesimple[0] = 1; //queues movement
-    newcommand = 1;}
+    newcommand = 1;
+  }
   
   if(option == 3){ //close door with fancy stepwise feedback
     uint8_t start = inputbuffer[1];
     uint8_t stop = inputbuffer[2];
     
-    closefancy(start, stop, S1_pulsetime);}
+    closefancy(start, stop, S1_pulsetime);
+  }
   
   if(option == 4){ //door feedback vibration
     Q_vibrate[1] = inputbuffer[1];  //steps
     Q_vibrate[2] = inputbuffer[2];  //speed
     Q_vibrate[3] = inputbuffer[3];  //repetitions
-    Q_vibrate[0] = 1;}}            //queues movement
+    Q_vibrate[0] = 1; //queues movement
+  }
+}            
 
 //I2C send instructions
 void sendData(){
@@ -507,4 +557,5 @@ void sendData(){
   
   sendbuffer[1] = sendbuffer[1] | ((S1_busy & 0x01) << 0);
   
-  Wire.write(sendbuffer,2);}
+  Wire.write(sendbuffer,2);
+}
